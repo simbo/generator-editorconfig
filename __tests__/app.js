beefe928e756ab78b9038574a0c3da87429bf76e
Simon Lepel <simbo@simbo.de>
2018-05-27 20:05:53 +0200

    refactor tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,30 +1,23 @@
 const path = require('path');
 const assert = require('yeoman-assert');
-const test = require('yeoman-test');
+const helpers = require('yeoman-test');
+
+const testApp = () => helpers.run(require.resolve('../generators/app'));
 
 describe('generator-editorconfig:app', () => {
-
-  it('creates `.editorconfig`', () => {
-    return test
-      .run(require.resolve('../generators/app'))
-      .then(() => {
-        assert.file(['.editorconfig']);
-      });
-  });
+  it('creates `.editorconfig`', () =>
+    testApp().then(() => {
+      assert.file(['.editorconfig']);
+    }));
 
   describe('with option `generateInto`', () => {
-
-    it('creates `.editorconfig` in desired path', () => {
-      return test
-        .run(require.resolve('../generators/app'))
+    it('creates `.editorconfig` in desired path', () =>
+      testApp()
         .withOptions({
           generateInto: 'foo'
         })
         .then(() => {
           assert.file(['foo/.editorconfig']);
-        });
-    });
-
+        }));
   });
-
 });
